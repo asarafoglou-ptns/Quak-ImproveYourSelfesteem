4a0c55717f667ab16fc59a2cc9074eeb7f89c420
yassieq <yasminhquak@gmail.com>
2024-05-24 22:16:37 +0200

    first use app function + added slider
    first use app function + added slider

--- ImproveYourSelfimage_initial/R/app.R
+++ ImproveYourSelfimage_initial/R/app.R
@@ -48,7 +48,11 @@ ui <- shiny::navbarPage(
   # Likelihood positive self-image
   shiny::navbarMenu("Positive self-image",
     shiny::tabPanel("Entry",
-    "Entry"
+      shiny::uiOutput("self_image_text"),
+      shiny::sliderInput("slider1", label = "Right
+        now, how likely do you think it is that that sentence is true?", min = 0, 
+        max = 100, value = 50),
+      shiny::actionButton("save_button2", "Submit"),
     ),
     shiny::tabPanel("Graph",
     "Graph"  
@@ -60,75 +64,31 @@ server <- function(input, output, session) {
   # Load background wordcloud function
   source("R/generate_wordcloud.R")
   source("R/dataloading.R")
+  source("R/save_whitebook_entry.R")
+  source("R/first_use_app.R")
   
   # Read the content of the introduction text file
   introduction_text <- readr::read_file("R/introduction.md")
   
-  # Check if first loading app
-  shiny::observe({
-    if (!file.exists("pos_selfimage.txt")) {
-      
-      # Show welcome screen to first time users
-      shiny::showModal(shiny::modalDialog(
-        title = "Welcome!",
-        HTML(markdown::markdownToHTML(introduction_text, template = FALSE)),
-        easyClose = FALSE,
-        footer = shiny::actionButton("ok", "ok")
-      ))
-    }
-  })
+  # Read the user-defined positive self-image from the text file
+  pos_selfimage <- if (file.exists("pos_selfimage.txt")) {
+    readr::read_file("pos_selfimage.txt")
+  } else {
+    "Not defined yet"
+  }
   
-  # Ask first time user for negative self-image
-  shiny::observeEvent(input$ok, {
-    shiny::showModal(shiny::modalDialog(
-      title = "Welcome!",
-      shiny::textInput("neg_selfimage", 
-                       "The first step is to describe your current negative 
-                       self-image. A negative self-image is a general, 
-                       negative statement about yourself starting with I, 
-                       for example 'I am not good enough'. How would you 
-                       describe your current negative self-image?"),
-      easyClose = FALSE,
-      footer = shiny::actionButton("ok2", "ok")
-    ))
-  })
-  
-  # Trigger next pop up window
-  shiny::observeEvent(input$ok2, {
-    neg_selfimage <- shiny::isolate(input$neg_selfimage)
-    
-    # Add negative self-image to txt file
-    if (nchar(neg_selfimage) > 0){
-      readr::write_lines(neg_selfimage, "neg_selfimage.txt")
-      
-      # Ask first time user to define desired positive self-image
-      shiny::showModal(shiny::modalDialog(
-        title = "Welcome!",
-        shiny::textInput("pos_selfimage", 
-                         "The next step is to define the positive self-image
-                         you want to strive towards. This is a positive general
-                         statement about yourself. It's easiest to think about
-                         the negative self-image you just formulated and rewrite
-                         it to a positive statment. For example, 'I am 
-                         worthless' becomes 'I am worthwhile'."),
-        easyClose = FALSE,
-        footer = shiny::actionButton("ok3", "ok")
-      ))
-    }
-  })
-  
-  # Handle the submission of the positive self-image
-  shiny::observeEvent(input$ok3, {
-    pos_selfimage <- isolate(input$pos_selfimage)
-    if (nchar(pos_selfimage) > 0) {
-      readr::write_lines(pos_selfimage, "pos_selfimage.txt")
-      removeModal()
-    }
-  })
+  # Call the first_use_app function
+  first_use_app(input, output, introduction_text)
   
   # Render the introduction text
   output$introduction <- shiny::renderUI({
     shiny::HTML(markdown::markdownToHTML(introduction_text, template = FALSE))
+  })
+  
+  # Render the self-image text
+  output$self_image_text <- renderUI({
+    p(paste("When you first started up this app 
+      you defined your desired self-image to be:", pos_selfimage, "."))
   })
   
   # Create empty data frames for if data does not exist
